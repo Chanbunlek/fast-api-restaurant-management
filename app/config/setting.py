e0e93539bbad380a6329014d7d8f8b54a2f4a6e7
chanbunlek <chanbunlek9@gmail.com>
2024-11-18 10:43:57 +0700

    fix: Can not find .env

--- app/config/setting.py
+++ app/config/setting.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     keycloak_client_secret: str = Field(..., env="KEYCLOAK_CLIENT_SECRET")
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
         env_file_encoding = "utf-8"
 
 settings = Settings()
